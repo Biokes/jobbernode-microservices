bba2fa528de21df23a1a5b9ffab84cf329c8aeb8
Biokes <106444765+Biokes@users.noreply.github.com>
2025-05-30 12:08:02 +0100

    [main] fix: resolve pom conflict

--- eureka-server/src/main/java/com/joberNode/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/joberNode/eureka_server/EurekaServerApplication.java
@@ -1,6 +1,10 @@
+package com.joberNode.eureka_server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 	public static void main(String[] args) {
